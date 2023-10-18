0c15a2d071601b9d22247527802412a74f5c16ae
alehoppai <aleh_belski@outlook.com>
2023-10-18 13:58:04 +0200

    Feat: AdditionalInfo component

---
+++ src/lib/AdditionalInfo.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Panel from "./Panel.svelte";
+
+
+    const additionalInfoData = [
+        { title: "Languages", text: "BY, EN, RU. Basic proficiency in Polish." },
+        { title: "Interests", text: "Passionate about video gaming, city explorations, and cherishing moments with my child." },
+    ]
+</script>
+
+<Panel>
+    <h3 class="text-xl font-bold">Additional Info</h3>
+    <ul>
+        {#each additionalInfoData as info}
+            <li class="my-4">
+                <strong>{info.title}:</strong>
+                <span>{info.text}</span>
+            </li>
+        {/each}
+    </ul>
+</Panel>
+
