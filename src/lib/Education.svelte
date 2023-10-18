ba08b543802ff1b223975015553cc97d36cbc66d
alehoppai <aleh_belski@outlook.com>
2023-10-18 13:58:14 +0200

    Feat: Education component

---
+++ src/lib/Education.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Panel from "./Panel.svelte";
+
+</script>
+
+<Panel bgEnabled>
+    <h3 class="text-xl font-bold">Education:</h3>
+    <ul>
+        <li class="my-4">High School Diploma</li>
+        <li class="my-4">Completed Military Service</li>
+    </ul>
+</Panel>
