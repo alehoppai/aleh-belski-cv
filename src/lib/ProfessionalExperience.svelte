c1e04fda3bff719e171effef1af1b2408acf6388
alehoppai <aleh_belski@outlook.com>
2023-10-18 20:00:06 +0200

    Feat: filtering jobs by tech

--- src/lib/ProfessionalExperience.svelte
+++ src/lib/ProfessionalExperience.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { Icon, Funnel } from "svelte-hero-icons";
     import Panel from "./Panel.svelte";
 
     const expData = [
@@ -24,7 +25,7 @@
             title: "Full-stack Developer, Instinctools / Oakley",
             timePeriod: "Apr 2017 - Jun 2017",
             description: "Assisted in developing a platform for selling customizable glasses, with a focus on lens customization.",
-            tech: "Java/Hybris, JavaScript, Gulp, Sass."
+            tech: "Java/Hybris, JQuery, Gulp, Sass."
         },
         {
             title: "Front-end Developer, Instinctools / Skype TripAdvisor Add-in",
@@ -47,51 +48,60 @@
         {
             title: "Front-End Developer, Instinctools / Lightning system",
             timePeriod: "Mar 2018 - May 2018",
-            description: "Developed tools for creating schemas to control lighting systems in buildings.",
+            description: "Developed tools for creating schemas for tablet app to control lighting systems in buildings.",
             tech: "React, sass, canvas."
         },
         {
             title: "Mobile Developer, Instinctools / Drop boy",
             timePeriod: "Jun 2018 - Aug 2018",
             description: "Developed a mobile app for courier delivery tracking using react-native, featuring text recognition for Bar and QR codes, and signature capturing.",
-            tech: "React-native."
+            tech: "Typescript, React-native, Redux, Firebase."
         },
         {
             title: "Full-stack Developer, Instinctools / Uber web",
             timePeriod: "Aug 2018 - June 2020",
             description: "Contributed to the main Uber website and internal microservices.\n Notable Contribution: /price-estimate",
-            tech: "Fusion.js, Custom Uber React framework, GoLang, Styletron, Storybook."
+            tech: "Flow.js, Fusion.js, Custom Uber React framework, Styletron, Storybook, Go, MySQL."
         },
         {
             title: "Full-stack Developer, Oxagile / Shindig",
             timePeriod: "Jul 2020 - March 2022",
             description: "Worked on a video chat platform supporting multiple speakers and numerous users.",
-            tech: "Angular 1.4, Node.js/Express/Nest.js, MySQL, Redis, AWS, Docker."
+            tech: "Typescript, Angular 1.4, Node.js/Express/Nest.js, MySQL, Redis, AWS, Docker."
         },
         {
             title: "Front-end Developer, Oxagile / Gousto",
             timePeriod: "Apr 2022 - Nov 2022",
             description: "Engaged in developing a food delivery website based on subscription.",
-            tech: "React/Redux, postcss, emotion, swr."
+            tech: "Typescript, React/Redux, postcss, emotion, swr."
         },
         {
             title: "Full-stack Developer, Oxagile / Opera",
             timePeriod: "Dec 2022 - June 2023",
             description: "Led the development of the web gaming platform Game Bites.",
-            tech: "Svelte-kit, Docker, Strapi."
+            tech: "Typescript, Svelte-kit, Docker, Strapi."
         },
         {
             title: "Front-end Developer, Oxagile / Opera",
             timePeriod: "Dec 2022 - June 2023",
             description: "Tasked with bug fixes for the Aria browser extension.",
-            tech: "React, Redux, Tailwindcss."
+            tech: "Typescript, React, Redux, Tailwindcss."
         }
     ]
+
+    let filterQuery = "";
 </script>
 
 <Panel>
-    <h3 class="text-xl font-bold">Professional Experience</h3>
-    {#each expData as job}
+    <div class="flex flex-row items-center justify-between">
+        <h3 class="text-xl font-bold">Professional Experience</h3>
+        <div class="flex flex-row items-center justify-between gap-2">
+            <Icon src={Funnel} class="w-4 h-4" />
+            <input placeholder="Project tech filter" type="text" class="border-b" bind:value={filterQuery}>
+        </div>
+    </div>
+
+    {#each filterQuery.length ? expData.filter(job => job.tech.toLowerCase().includes(filterQuery.toLowerCase())) : expData as job}
         <table class="table-fixed border-collapse text-left my-4">
             <thead>
                 <tr>
