d4cd0f9c3af535a1cf3f943ca92a0a690ac98a33
alehoppai <aleh_belski@outlook.com>
2023-10-21 08:32:28 +0200

    Fix: changing about

--- src/lib/Summary.svelte
+++ src/lib/Summary.svelte
@@ -5,7 +5,7 @@
 <Panel>
 	<h3 class="text-xl font-bold">Summary:</h3>
 	<p>
-		Dynamic Fullstack JavaScript Developer with nearly a decade of experience since January
+		Fullstack JavaScript Developer with nearly a decade of experience since January
 		2014. My journey began with Angular and Ruby, transitioning through diverse projects
 		involving Node.js/Express, React apps, React-native, and even Golang. Having worked in both
 		frontend and backend roles, I've collaborated with globally renowned companies such as Uber,
